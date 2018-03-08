0783e43611d800b6b00196bee4cf3e974d9f0884
Leo Liu <wei.liu@zuora.com>
2018-03-08 17:15:09 +0800

    add deploy parameter check

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+if [ $# -ne 1 ]; then
+  printf "\e[34;1mError: Need to provide commit message as argument.\e[0m \n"
+  printf "Usage:\n"
+  printf "\t./deploy.sh 'commit message'\n"
+  exit 1
+fi
+
 # Add all changes
 git add .
 
